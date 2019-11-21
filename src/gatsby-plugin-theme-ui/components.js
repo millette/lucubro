afa5f039d8534e3514171e8fcb44cac4092ff5ee
Robin Millette <robin@millette.info>
2019-11-21 18:33:34 -0500

    Remove main layout from mdx components.

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,11 +1,7 @@
 // npm
 import Prism from "@theme-ui/prism"
 
-// self
-import Layout from "../shared/layout"
-
 export default {
-  wrapper: Layout,
   pre: ({ children }) => children,
   code: Prism,
 }
