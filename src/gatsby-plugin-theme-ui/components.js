c02a49f2cd4764a76f5b0a33fcf9adfd4a29ab00
Robin Millette <robin@millette.info>
2019-11-21 18:05:06 -0500

    Add layout as mdx wrapper.

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,7 +1,11 @@
 // npm
 import Prism from "@theme-ui/prism"
 
+// self
+import Layout from "../shared/layout"
+
 export default {
+  wrapper: Layout,
   pre: ({ children }) => children,
   code: Prism,
 }
