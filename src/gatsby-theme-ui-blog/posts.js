195051c792d4939f9d7537856bffaee24ef89a91
Robin Millette <robin@millette.info>
2019-11-21 16:35:29 -0500

    Healthier posts.

--- src/gatsby-theme-ui-blog/posts.js
+++ src/gatsby-theme-ui-blog/posts.js
@@ -1,16 +1,20 @@
 /** @jsx jsx */
-import { jsx, Styled } from 'theme-ui'
-import { Link } from 'gatsby'
-import { Layout } from 'gatsby-theme-ui-layout'
+// npm
+import { jsx, Styled } from "theme-ui"
+import { Link } from "gatsby"
+import { Layout } from "gatsby-theme-ui-layout"
+import PropTypes from "prop-types"
 
-export default ({ posts, ...props }) => (
+const Posts = ({ posts, ...props }) => (
   <Styled.root>
     <Layout {...props}>
       <h1>Hello world!</h1>
 
       <ul>
-        <li><Link to="/style-guide">Style guide</Link></li>
-        {posts.map(post => (
+        <li>
+          <Link to="/style-guide">Style guide</Link>
+        </li>
+        {posts.map((post) => (
           <li key={post.id}>
             <Link to={post.slug}>{post.title}</Link>
           </li>
@@ -19,3 +23,9 @@ export default ({ posts, ...props }) => (
     </Layout>
   </Styled.root>
 )
+
+Posts.propTypes = {
+  posts: PropTypes.array,
+}
+
+export default Posts
