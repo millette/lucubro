3973aa435b6403cd04f3f998c2e2514e41dbfa7e
Robin Millette <robin@millette.info>
2019-11-21 15:59:07 -0500

    Use theme-ui preset colors for prism.

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,7 +1,10 @@
 // npm
 import deepmerge from "deepmerge"
 import { tosh as theme } from '@theme-ui/presets'
-import prism from '@theme-ui/prism/presets/github.json'
+// import prism from '@theme-ui/prism/presets/github.json'
+import prism from '@theme-ui/prism/presets/theme-ui'
+
+// export default {}
 
 export default deepmerge(theme, {
   styles: {
