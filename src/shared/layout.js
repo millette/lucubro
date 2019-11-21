f15a4f83aeafa043e3bd8e9a8e1df60616478618
Robin Millette <robin@millette.info>
2019-11-21 16:09:40 -0500

    Add light/dark mode toggle.

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -1,14 +1,31 @@
 // npm
 import React from 'react'
 import { Link } from 'gatsby'
+import { useColorMode } from 'theme-ui'
+
 
 // self
 import Head from './head'
 
-export default (props) => (
+export default (props) => {
+const [colorMode, setColorMode] = useColorMode()
+
+return (
   <>
     <Head {...props} />
     <h1><Link to="/" >My own ham</Link></h1>
+
+      <button
+        onClick={() => {
+          setColorMode(colorMode === 'default' ? 'dark' : 'default')
+        }}>
+        Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+      </button>
+
+
+
     {props.children}
   </>
 )
+}
+
