4aef28ac896c699f05840c946a6784afb3f2ff7a
Robin Millette <robin@millette.info>
2019-11-21 16:38:34 -0500

    Healthier head.

--- src/shared/head.js
+++ src/shared/head.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+// npm
+import React from "react"
+import { Helmet } from "react-helmet"
+import PropTypes from "prop-types"
 
-export default ({ lang = 'en-us', title, excerpt, ...props }) => (
+const Head = ({ lang = "en-us", title, excerpt, ...props }) => (
   <Helmet
     {...props}
     htmlAttributes={{
       lang,
-    }}>
+    }}
+  >
     {title && <title>{title}</title>}
     {excerpt && <meta name="description" content={excerpt} />}
   </Helmet>
 )
+
+Head.propTypes = {
+  lang: PropTypes.string,
+  title: PropTypes.string,
+  excerpt: PropTypes.string,
+}
+
+export default Head
