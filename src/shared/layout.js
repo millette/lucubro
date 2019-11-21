ebceca184c29d731cee48f6dca9ca4129c335603
Robin Millette <robin@millette.info>
2019-11-21 16:40:35 -0500

    Healthier layout.

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -1,31 +1,37 @@
 // npm
-import React from 'react'
-import { Link } from 'gatsby'
-import { useColorMode } from 'theme-ui'
-
+import React from "react"
+import { Link } from "gatsby"
+import { useColorMode } from "theme-ui"
+import PropTypes from "prop-types"
 
 // self
-import Head from './head'
+import Head from "./head"
 
-export default (props) => {
-const [colorMode, setColorMode] = useColorMode()
+const Layout = (props) => {
+  const [colorMode, setColorMode] = useColorMode()
 
-return (
-  <>
-    <Head {...props} />
-    <h1><Link to="/" >My own ham</Link></h1>
+  return (
+    <>
+      <Head {...props} />
+      <h1>
+        <Link to="/">My own ham</Link>
+      </h1>
 
       <button
         onClick={() => {
-          setColorMode(colorMode === 'default' ? 'dark' : 'default')
-        }}>
-        Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+          setColorMode(colorMode === "default" ? "dark" : "default")
+        }}
+      >
+        Toggle {colorMode === "default" ? "Dark" : "Light"}
       </button>
 
-
-
-    {props.children}
-  </>
-)
+      {props.children}
+    </>
+  )
 }
 
+Layout.propTypes = {
+  children: PropTypes.any,
+}
+
+export default Layout
