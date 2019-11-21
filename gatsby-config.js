1ddb53e2c61e1d61e83fe1d8c07ec0773713da04
Robin Millette <robin@millette.info>
2019-11-21 15:46:44 -0500

    Add site metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 'use strict'
 
 module.exports = {
+  siteMetadata: {
+    title: `I still have a project`,
+    author: `Robin Millette`,
+    description: `My journey coding hundreds of projects.`,
+    siteUrl: `http://lucubro.waglo.com/`,
+    social: {
+      // twitter: `RoLLodeQc`,
+      name: `millette@mastodon`,
+      url: `https://mastodon.fedi.quebec/@millette`,
+    },
+  },
   plugins: [
     'gatsby-theme-ui-blog',
     'gatsby-theme-style-guide',
