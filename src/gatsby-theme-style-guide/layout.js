b859823cc26d0ed3419dfad105a79f1803583063
Robin Millette <robin@millette.info>
2019-11-21 18:34:26 -0500

    Simplify style guide layout.

--- src/gatsby-theme-style-guide/layout.js
+++ src/gatsby-theme-style-guide/layout.js
@@ -1,15 +1,4 @@
-/** @jsx jsx */
-
-// npm
-import { jsx, Styled } from "theme-ui"
-
 // self
-import Layout2 from "../shared/layout"
-
-const Layout = (props) => (
-  <Styled.root>
-    <Layout2 {...props} />
-  </Styled.root>
-)
+import Layout from "../shared/layout"
 
 export default Layout
