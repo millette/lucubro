bbb19cda74d0ac36d142e4149c51b0691cc71d25
Robin Millette <robin@millette.info>
2019-11-21 20:55:14 -0500

    Find proper title.

--- src/shared/head.js
+++ src/shared/head.js
@@ -3,17 +3,26 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 
-const Head = ({ lang = "en-us", title, excerpt, ...props }) => (
-  <Helmet
-    {...props}
-    htmlAttributes={{
-      lang,
-    }}
-  >
-    {title && <title>{title}</title>}
-    {excerpt && <meta name="description" content={excerpt} />}
-  </Helmet>
-)
+const Head = ({ lang = "en-us", title, excerpt, ...props }) => {
+  if (!title) {
+    if (props._frontmatter && props._frontmatter.title) {
+      title = props._frontmatter.title
+    } else if (props.data && props.data.site.siteMetadata.title) {
+      title = props.data.site.siteMetadata.title
+    }
+  }
+  return (
+    <Helmet
+      {...props}
+      htmlAttributes={{
+        lang,
+      }}
+    >
+      <title>{title || "UNEXPECTED --- MISSING TITLE"}</title>
+      {excerpt && <meta name="description" content={excerpt} />}
+    </Helmet>
+  )
+}
 
 Head.propTypes = {
   lang: PropTypes.string,
