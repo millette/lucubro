35c74b94bee6f4caf4b90a08937f743a0dd5dc13
Robin Millette <robin@millette.info>
2019-11-21 21:27:09 -0500

    Use package.json in gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,19 @@
-'use strict'
+"use strict"
+
+const { name, description, author } = require("./package.json")
 
 module.exports = {
+  pathPrefix: `/${name}`,
   siteMetadata: {
-    title: `I still have a project`,
-    author: `Robin Millette`,
-    description: `My journey coding hundreds of projects.`,
-    siteUrl: `http://lucubro.waglo.com/`,
+    title: "I still have a project",
+    author: author.name,
+    description,
+    siteUrl: `https://millette.github.io/${name}`,
     social: {
       // twitter: `RoLLodeQc`,
       name: `millette@mastodon`,
       url: `https://mastodon.fedi.quebec/@millette`,
     },
   },
-  plugins: [
-    'gatsby-theme-ui-blog',
-    'gatsby-theme-style-guide',
-  ]
+  plugins: ["gatsby-theme-ui-blog", "gatsby-theme-style-guide"],
 }
-
