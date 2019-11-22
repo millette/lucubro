d567c32600d070654fbc9ac597c099dca653d50e
Robin Millette <robin@millette.info>
2019-11-21 23:19:04 -0500

    Use styled link in layout.

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -2,7 +2,6 @@
 
 // npm
 import React from "react" // eslint-disable-line no-unused-vars
-import { Link } from "gatsby"
 import {
   jsx,
   useColorMode,
@@ -19,91 +18,94 @@ import PropTypes from "prop-types"
 
 // self
 import Head from "./head"
+import Link from "./link"
 
 const OwnLayout = (props) => {
   const [colorMode, setColorMode] = useColorMode()
 
   return (
-    <Layout>
-      <Head {...props} />
-      <Header>
-        <Container>
-          <Flex
-            sx={{
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Box>
-              <Styled.h1>
-                <Link to="/">I still have a project</Link>
-              </Styled.h1>
-            </Box>
-            <Box>
-              <button
-                onClick={() => {
-                  setColorMode(colorMode === "default" ? "dark" : "default")
-                }}
-              >
-                Toggle {colorMode === "default" ? "Dark" : "Light"}
-              </button>
-            </Box>
-          </Flex>
-          <Flex
-            sx={{
-              justifyContent: "space-evenly",
-            }}
-          >
-            <Box>
-              <Link to="/about">About</Link>
-            </Box>
-            <Box>
-              <Link to="/style-guide">Style guide</Link>
-            </Box>
-          </Flex>
-        </Container>
-      </Header>
-      <Main>
-        <Container>
-          {props.children}
+    <Styled.root>
+      <Layout>
+        <Head {...props} />
+        <Header>
+          <Container>
+            <Flex
+              sx={{
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <Styled.h1>
+                  <Link href="/">I still have a project (v1)</Link>
+                </Styled.h1>
+              </Box>
+              <Box>
+                <button
+                  onClick={() => {
+                    setColorMode(colorMode === "default" ? "dark" : "default")
+                  }}
+                >
+                  Toggle {colorMode === "default" ? "Dark" : "Light"}
+                </button>
+              </Box>
+            </Flex>
+            <Flex
+              sx={{
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Box>
+                <Link to="/about">About</Link>
+              </Box>
+              <Box>
+                <Link to="/style-guide">Style guide</Link>
+              </Box>
+            </Flex>
+          </Container>
+        </Header>
+        <Main>
+          <Container>
+            {props.children}
 
-          {props.data && (props.data.previous || props.data.next) && (
-            <>
-              <Styled.h3>More articles</Styled.h3>
+            {props.data && (props.data.previous || props.data.next) && (
+              <>
+                <Styled.h3>More articles</Styled.h3>
 
-              <Flex
-                sx={{
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                }}
-              >
-                <Box>
-                  {props.data && props.data.previous && (
-                    <Link to={props.data.previous.slug}>
-                      {props.data.previous.title}
-                    </Link>
-                  )}
-                </Box>
-                <Box>
-                  {props.data && props.data.next && (
-                    <Link to={props.data.next.slug}>
-                      {props.data.next.title}
-                    </Link>
-                  )}
-                </Box>
-              </Flex>
-            </>
-          )}
-        </Container>
-      </Main>
+                <Flex
+                  sx={{
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Box>
+                    {props.data && props.data.previous && (
+                      <Link to={props.data.previous.slug}>
+                        {props.data.previous.title}
+                      </Link>
+                    )}
+                  </Box>
+                  <Box>
+                    {props.data && props.data.next && (
+                      <Link to={props.data.next.slug}>
+                        {props.data.next.title}
+                      </Link>
+                    )}
+                  </Box>
+                </Flex>
+              </>
+            )}
+          </Container>
+        </Main>
 
-      <Footer>
-        <Container>
-          <Styled.hr />
-          <Styled.p>Footer...</Styled.p>
-        </Container>
-      </Footer>
-    </Layout>
+        <Footer>
+          <Container>
+            <Styled.hr />
+            <Styled.p>Footer...</Styled.p>
+          </Container>
+        </Footer>
+      </Layout>
+    </Styled.root>
   )
 }
 
