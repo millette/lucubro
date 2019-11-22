c93c0d2fcbb286b70165228cffcf9dd11f22a5fe
Robin Millette <robin@millette.info>
2019-11-22 11:13:52 -0500

    Use futura theme preset.

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,6 +1,9 @@
 // npm
 import deepmerge from "deepmerge"
-import { tosh as theme } from "@theme-ui/presets"
+import { future as theme } from "@theme-ui/presets"
+// import { system as theme } from "@theme-ui/presets"
+// import { future as theme } from "@theme-ui/presets"
+// import { tosh as theme } from "@theme-ui/presets"
 import prism from "@theme-ui/prism/presets/theme-ui"
 
 export default deepmerge(theme, {
