094f5e9d370ea7ce0642b3fa609e6d5c7b4dc162
Robin Millette <robin@millette.info>
2019-11-21 20:54:42 -0500

    Add title to style guide page.

--- src/gatsby-theme-style-guide/layout.js
+++ src/gatsby-theme-style-guide/layout.js
@@ -1,4 +1,9 @@
+// npm
+import React from "react"
+
 // self
-import Layout from "../shared/layout"
+import Layout2 from "../shared/layout"
+
+const Layout = (props) => <Layout2 {...props} title="Style Guide" />
 
 export default Layout
