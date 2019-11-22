911c820354733be26f2486c6d725c31548b2c2e9
Robin Millette <robin@millette.info>
2019-11-21 21:30:56 -0500

    Healthier post.

--- src/gatsby-theme-ui-blog/post.js
+++ src/gatsby-theme-ui-blog/post.js
@@ -2,22 +2,26 @@
 
 // npm
 import { jsx, Styled } from "theme-ui"
+import PropTypes from "prop-types"
 
 // self
 import Layout from "../shared/layout"
 
-export default ({
-  title,
-  date,
-  excerpt,
-  children,
-  keywords,
-  tags,
-  ...props
-}) => (
+const Post = ({ title, date, excerpt, children, keywords, tags, ...props }) => (
   <Layout title={title} excerpt={excerpt} {...props}>
     <Styled.h1>{title}</Styled.h1>
     <div>{date}</div>
     {children}
   </Layout>
 )
+
+Post.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  excerpt: PropTypes.string,
+  keywords: PropTypes.array,
+  tags: PropTypes.array,
+  children: PropTypes.any,
+}
+
+export default Post
