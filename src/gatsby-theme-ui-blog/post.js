43d9065ba8234904d8c79ca049b0a5124b539fb2
Robin Millette <robin@millette.info>
2019-11-24 13:08:36 -0500

    Stub state field.

--- src/gatsby-theme-ui-blog/post.js
+++ src/gatsby-theme-ui-blog/post.js
@@ -7,10 +7,20 @@ import PropTypes from "prop-types"
 // self
 import Layout from "../components/layout"
 
-const Post = ({ title, date, excerpt, children, keywords, tags, ...props }) => (
+const Post = ({
+  state,
+  title,
+  date,
+  excerpt,
+  children,
+  keywords,
+  tags,
+  ...props
+}) => (
   <Layout title={title} excerpt={excerpt} {...props}>
     <Styled.h1>{title}</Styled.h1>
     <div>{date}</div>
+    <Styled.h2>*{state}*</Styled.h2>
     {children}
   </Layout>
 )
