cd1dec4d2d9c2913aad79003f36f887c05600bc8
Robin Millette <robin@millette.info>
2019-11-24 13:08:52 -0500

    Improve frontpage.

--- src/gatsby-theme-ui-blog/posts.js
+++ src/gatsby-theme-ui-blog/posts.js
@@ -1,24 +1,47 @@
 /** @jsx jsx */
 
 // npm
-import { jsx, Styled } from "theme-ui"
+import { jsx, Styled, Flex, Box } from "theme-ui"
 import PropTypes from "prop-types"
 
 // self
 import Layout from "../components/layout"
 import Link from "../components/link"
+import Intro from "../components/intro"
 
 const Posts = ({ posts, ...props }) => (
   <Layout {...props}>
-    <Styled.h1>Hello world!</Styled.h1>
+    <Flex>
+      <Box
+        sx={{
+          width: ["100%", "66.67%"],
+        }}
+      >
+        <Intro />
+        <Styled.p>
+          Find out more on the <Link to="about">about page</Link>.
+        </Styled.p>
+        <Styled.p sx={{ textAlign: "right" }}>
+          Signed "Your host, <Styled.b>Robin Millette</Styled.b>"
+        </Styled.p>
+      </Box>
+      <Box
+        sx={{
+          textAlign: "right",
+          width: ["100%", "33.33%"],
+        }}
+      >
+        <Styled.h2>Most recent first</Styled.h2>
 
-    <Styled.ul>
-      {posts.map((post) => (
-        <Styled.li key={post.id}>
-          <Link to={post.slug}>{post.title}</Link>
-        </Styled.li>
-      ))}
-    </Styled.ul>
+        <Styled.ul>
+          {posts.map((post) => (
+            <Styled.li key={post.id}>
+              <Link to={post.slug}>{post.title}</Link>
+            </Styled.li>
+          ))}
+        </Styled.ul>
+      </Box>
+    </Flex>
   </Layout>
 )
 
