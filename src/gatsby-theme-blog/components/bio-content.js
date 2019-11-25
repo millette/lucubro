151ca2f54ea70163d0b18b4222a5bf116d259232
Robin Millette <robin@millette.info>
2019-11-25 18:13:29 -0500

    Healthier bio-content.

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,14 +1,22 @@
 // npm
 import React from "react"
 import { Styled } from "theme-ui"
+import PropTypes from "prop-types"
 
 // self
 import Link from "../../components/external-link"
 
-export default ({ author, siteUrl }) => (
+const BioContent = ({ author, siteUrl }) => (
   <Styled.p>
     Words by <Link to={siteUrl}>{author}</Link>.
     <br />
     Change me. This is all quite default.
   </Styled.p>
 )
+
+BioContent.propTypes = {
+  author: PropTypes.string.isRequired,
+  siteUrl: PropTypes.string.isRequired,
+}
+
+export default BioContent
