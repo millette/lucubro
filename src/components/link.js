9aa90f8780336aa073e718775e246c22ef786196
Robin Millette <robin@millette.info>
2019-11-25 18:08:09 -0500

    Cleanup link.

--- src/components/link.js
+++ src/components/link.js
@@ -1,18 +1,16 @@
-/** @jsx jsx */
-
 // npm
-import React from "react" // eslint-disable-line no-unused-vars
+import React from "react"
 import { Link } from "gatsby"
-import { jsx, Styled } from "theme-ui"
+import { Styled } from "theme-ui"
 import PropTypes from "prop-types"
 
 const StyledLink = ({ to, ...props }) => {
   if (to.slice(-1) !== "/") to += "/"
   return <Styled.a {...props} to={to} as={Link} />
 }
+
 StyledLink.propTypes = {
-  href: PropTypes.string,
-  to: PropTypes.string,
+  to: PropTypes.string.isRequired,
 }
 
 export default StyledLink
