619d299bb1641f8107f1bd3223a26f580ccfbc87
Robin Millette <robin@millette.info>
2019-11-25 20:14:33 -0500

    Layout footer.

--- src/gatsby-theme-blog/components/home-footer.js
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -1,33 +1,35 @@
 // npm
 import React, { Fragment } from "react"
-import { Styled, css } from "theme-ui"
+import { Styled, css, Footer, Container } from "theme-ui"
 import PropTypes from "prop-types"
 
-const Footer = ({ socialLinks }) => (
-  <footer
+const HomeFooter = ({ socialLinks }) => (
+  <Footer
     css={css({
       mt: 4,
       pt: 3,
     })}
   >
-    © 2019 Robin Millette. Powered by Gatsby &bull;{" "}
-    {socialLinks.map((platform, i, arr) => (
-      <Fragment key={platform.url}>
-        <Styled.a
-          href={platform.url}
-          target="_blank"
-          rel="me noopener noreferrer"
-        >
-          {platform.name}
-        </Styled.a>
-        {arr.length - 1 !== i && <> &bull; </>}
-      </Fragment>
-    ))}
-  </footer>
+    <Container>
+      © 2019 Robin Millette. Powered by Gatsby &bull;{" "}
+      {socialLinks.map((platform, i, arr) => (
+        <Fragment key={platform.url}>
+          <Styled.a
+            href={platform.url}
+            target="_blank"
+            rel="me noopener noreferrer"
+          >
+            {platform.name}
+          </Styled.a>
+          {arr.length - 1 !== i && <> &bull; </>}
+        </Fragment>
+      ))}
+    </Container>
+  </Footer>
 )
 
-Footer.propTypes = {
+HomeFooter.propTypes = {
   socialLinks: PropTypes.array,
 }
 
-export default Footer
+export default HomeFooter
