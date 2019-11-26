459b0e6956014f08caacbef0c382c0dde127f47e
Robin Millette <robin@millette.info>
2019-11-26 14:50:32 -0500

    Healthier mdx layout.

--- src/components/mdx-layout.js
+++ src/components/mdx-layout.js
@@ -1,15 +1,17 @@
 // npm
 import React from "react"
 import { Main, Container } from "theme-ui"
-import SEO from "gatsby-theme-blog/src/components/seo"
+import Seo from "gatsby-theme-blog/src/components/seo"
+import PropTypes from "prop-types"
 
 // self
 import Layout from "../gatsby-theme-blog/components/layout"
 import PostFooter from "../gatsby-theme-blog/components/post-footer"
 
+// FIXME: hardcoded title and description...
 const MdxLayout = ({ location, children, _frontmatter: { title } }) => (
   <Layout location={location} title="I still have a project">
-    <SEO
+    <Seo
       title={title}
       description="My 30 year journey coding hundreds of projects."
     />
@@ -19,5 +21,11 @@ const MdxLayout = ({ location, children, _frontmatter: { title } }) => (
     <PostFooter />
   </Layout>
 )
+
+MdxLayout.propTypes = {
+  location: PropTypes.object,
+  children: PropTypes.any.isRequired,
+  _frontmatter: PropTypes.object.isRequired,
+}
 
 export default MdxLayout
