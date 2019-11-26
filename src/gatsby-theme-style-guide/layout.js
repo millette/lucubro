4da5b2d0f19cc731528f3f215a0845066e97d9d1
Robin Millette <robin@millette.info>
2019-11-26 15:20:44 -0500

    Healthier style guide layout.

--- src/gatsby-theme-style-guide/layout.js
+++ src/gatsby-theme-style-guide/layout.js
@@ -1,8 +1,16 @@
+// npm
+import React from "react"
+import PropTypes from "prop-types"
+
 // self
 import MdxLayout from "../components/mdx-layout"
 
 const StyleGuideLayout = ({ children }) => (
-  <MdxLayout _frontmatter={{ title: "Style Guide" }} children={children} />
+  <MdxLayout _frontmatter={{ title: "Style Guide" }}>{children}</MdxLayout>
 )
+
+StyleGuideLayout.propTypes = {
+  children: PropTypes.any.isRequired,
+}
 
 export default StyleGuideLayout
