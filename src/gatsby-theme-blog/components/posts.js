fa1b605cd8bf80a374226d622143191400c2a4fe
Robin Millette <robin@millette.info>
2019-11-25 20:15:09 -0500

    Layout posts.

--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -1,6 +1,6 @@
 // npm
 import React, { Fragment } from "react"
-import { Styled, css } from "theme-ui"
+import { Styled, css, Container, Main } from "theme-ui"
 import Layout from "gatsby-theme-blog/src/components/layout"
 import SEO from "gatsby-theme-blog/src/components/seo"
 
@@ -10,36 +10,38 @@ import Link from "../../components/link"
 
 const Posts = ({ location, posts, siteTitle, socialLinks }) => (
   <Layout location={location} title={siteTitle}>
-    <main>
-      {posts.map(({ node }) => {
-        const title = node.title || node.slug
-        const keywords = node.keywords || []
-        return (
-          <Fragment key={node.slug}>
-            <SEO title="Home" keywords={keywords} />
-            <div>
-              <Styled.h2
-                css={css({
-                  mb: 1,
-                })}
-              >
-                <Link
-                  as={Link}
-                  css={{
-                    textDecoration: `none`,
-                  }}
-                  to={node.slug}
+    <Main>
+      <Container>
+        {posts.map(({ node }) => {
+          const title = node.title || node.slug
+          const keywords = node.keywords || []
+          return (
+            <Fragment key={node.slug}>
+              <SEO title="Home" keywords={keywords} />
+              <div>
+                <Styled.h2
+                  css={css({
+                    mb: 1,
+                  })}
                 >
-                  {title}
-                </Link>
-              </Styled.h2>
-              <small>{node.date}</small>
-              <Styled.p>{node.excerpt}</Styled.p>
-            </div>
-          </Fragment>
-        )
-      })}
-    </main>
+                  <Link
+                    as={Link}
+                    css={{
+                      textDecoration: `none`,
+                    }}
+                    to={node.slug}
+                  >
+                    {title}
+                  </Link>
+                </Styled.h2>
+                <small>{node.date}</small>
+                <Styled.p>{node.excerpt}</Styled.p>
+              </div>
+            </Fragment>
+          )
+        })}
+      </Container>
+    </Main>
     <Footer socialLinks={socialLinks} />
   </Layout>
 )
