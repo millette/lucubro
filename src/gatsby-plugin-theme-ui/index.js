8768b79b4b47949728f67da4618ff8249b215a83
Robin Millette <robin@millette.info>
2019-11-27 16:17:51 -0500

    Add all color palettes to theme.

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,6 +4,11 @@ import deepmerge from "deepmerge"
 import { future as theme } from "@theme-ui/presets"
 import prism from "@theme-ui/prism/presets/theme-ui"
 
+// self
+import themes from "./themes"
+
+themes.forEach((t, i) => (theme.colors.modes[`custom-${i}`] = t))
+
 export default deepmerge(theme, {
   useColorSchemeMediaQuery: true,
   styles: {
